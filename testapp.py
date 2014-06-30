60b31f2f1567a3fc02cb8baa7a36f396475f14dd
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-06-30 11:03:17 -0700

    Updated test app to search places near theLinq

--- testapp.py
+++ testapp.py
@@ -8,8 +8,23 @@ for x in range (0,5):
     keyFile.readline()
 access_token = keyFile.readline().rstrip()
 
-# Displays the url of all popular images
 api = InstagramAPI(access_token=access_token)
+
+# Displays the url of all popular images
+"""
 popular_media = api.media_popular(count=20)
 for media in popular_media:
     print media.images['standard_resolution'].url
+"""
+# Displays links of all photos at The Linq
+# TODO: Get images from these locations and output to html file?
+# theLinqImages = api.location_search(lat=36.117590,lng=-115.171589, distance=2500)
+theLinqPlaces = api.location_search(lat=36.1,lng=-115.1, distance=2500)
+print theLinqPlaces
+for eachLocation in theLinqPlaces:
+    print eachLocation.id
+
+"""
+for media in theLinqImages:
+    print media.images['standard_resolution'].url
+"""
