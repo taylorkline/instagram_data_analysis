8de20b84a19464d23bcfbc74453a481e2cea3a1f
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-07-10 11:46:06 -0700

    Added a bit more verbosity.

--- followerLatLong.py
+++ followerLatLong.py
@@ -4,13 +4,14 @@
 # Setting variables in ALL_CAPS is your responsibility.
 from instagram.client import InstagramAPI
 import os
+from time import sleep
 
 # output file to be used for html output and opened in web browser
 outputFilename = 'followers.data'
 outputFile = open(outputFilename, 'w')
 
 # reads access token from specified line in keyFile
-ACCESS_TOKEN_LINE=6
+ACCESS_TOKEN_LINE=1
 keyFilename = 'keys'
 keyFile = open(keyFilename, 'r')
 for x in range (0,ACCESS_TOKEN_LINE-1):
@@ -19,19 +20,26 @@ access_token = keyFile.readline().rstrip()
 
 # user specified variables to influence search
 USERNAME = 'thelinq'
-MAXPAGES = 100 #maximum number of approximately 100-user pages to process
+MAXPAGES = 3 #maximum number of approximately 100-user pages to process
 # Note: This number is approximate, as some pages do not have the full 100 users
 
 api = InstagramAPI(access_token=access_token)
 
+privateUsers = 0
+publicUsers = 0
+
 # accepts a userID and gets the last location of the user based on recent photo, if available
 def getLastLocation(userID):
+    global privateUsers
+    global publicUsers
     try:
         userFeed = api.user_recent_media(user_id=userID)[0]
+        publicUsers += 1
         #TODO: get lat / long of most recent user feed
         print userFeed
     except Exception:
         print "User is set to private."
+        privateUsers += 1
 
 # Determines the userID from the username given
 userID = api.user_search(USERNAME)[0].id
@@ -47,10 +55,11 @@ while nextURL and counter < (MAXPAGES): #paginate until there are no more URLs o
     counter += 1
 totalFollowers = tuple(totalFollowers) #convert back to immutable tuple
 print "Found " + str(len(totalFollowers)) + " followers."
-# TODO: Print (out of xxxx followers)
+print "Finding most recent Lat/Long of each follower."
+sleep(2)
 
 # Now that we've got the followers, find their most recent photo
 for eachFollower in totalFollowers:
     getLastLocation(eachFollower.id)
 
-    #TODO: Print out stats of how many followers there are vs how much location data is available
+#TODO: Print out stats of how many followers there are vs how much location data is available
