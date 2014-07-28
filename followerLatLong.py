791b123b4ee04623c9a0f279bb4d7abe349dcd00
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-07-28 14:58:25 -0700

    Saving data into JSON for leaflt importing.

--- followerLatLong.py
+++ followerLatLong.py
@@ -19,13 +19,14 @@ access_token = keyFile.readline().rstrip()
 
 # user specified variables to influence search
 USERNAME = 'thelinq'
-MAXPAGES = 3 #maximum number of approximately 100-user pages to process
-MAXTRIES = 4 #number of pictures to go through on users' timelines to get location
+MAXPAGES = 10 #maximum number of approximately 100-user pages to process
 # Note: This number is approximate, as some pages do not have the full 100 users
+MAXTRIES = 5 #number of pictures to go through on users' timelines to attempt to find location
+#upper limit is 20 pictures to go through
 
 # output file to be used for html output and opened in web browser
 currentTime = str(localtime().tm_year) + '-' + str(localtime().tm_mon) + '-' + str(localtime().tm_mday) + '-' + str(localtime().tm_hour) + '-' + str(localtime().tm_sec)
-outputFilename = 'output_' + USERNAME + '_' + currentTime +  '.dat'
+outputFilename = 'output_' + USERNAME + '_' + currentTime +  '.js'
 outputDirectory = os.path.dirname(os.path.realpath(__file__)) + '/LatLongData/'
 try:
         os.makedirs(outputDirectory)
@@ -40,6 +41,10 @@ api = InstagramAPI(access_token=access_token)
 # track how many users we could get location from
 publicUsers = 0
 
+# initiates the .js data file
+def initiateOutput():
+    outputFile.write("var heatmapData = [")
+
 # accepts a userID and gets the last location of the user based on recent photo, if available
 """
 Example lat/long search:
@@ -53,16 +58,26 @@ def getLastLocation(userID):
         print "\nUser's feed contains: " + str(len(userFeed)) + " Media."
 
         #Search through a user's feed for location
+        tries = 0
         for index in userFeed:
+            if (tries >= MAXTRIES):
+                break
             print "Checking index of feed: " + str(index)
             try:
                 print "Found location: " + str(api.media(index.id).location.point)
+                #Conclude the last entry and prepare for the next
+                if (publicUsers > 0):
+                    outputFile.write("\n},")
+                outputFile.write("\n{\n\tlat: " + str(api.media(index.id).location.point.latitude) + 
+                        ",\n\tlon: " + str(api.media(index.id).location.point.longitude) + ",\n\t" +
+                        "value: 1")
+                outputFile.flush()
                 publicUsers += 1
                 break
-                #TODO: Stop if we find a location
-            #handle error if photo has no lat/long
+            #handle error if media has no lat/long
             except AttributeError:
                 pass
+            tries += 1
     #handle error if user has whole profile private
     except InstagramAPIError as e:
         print "\nUser is set to private."
@@ -72,6 +87,14 @@ def getLastLocation(userID):
         #TODO: Avoid IndexError with while loop
         pass
     """
+
+# concludes the .js data file
+def concludeOutput():
+    outputFile.write("\n}\n];")
+    outputFile.close()
+
+initiateOutput()
+print "Saving lat/long data to file:\n" + outputFile.name
 
 # Determines the userID from the username given
 userID = api.user_search(USERNAME)[0].id
@@ -97,4 +120,4 @@ percentage = int(round(float(publicUsers) / len(totalFollowers) * 100))
 print str(publicUsers) + " followers have location enabled (" + str(percentage) + "%) and " \
         + str(len(totalFollowers) - publicUsers) + " followers have no location."
 
-#TODO: Print out stats of how many followers there are vs how much location data is available
+concludeOutput()
