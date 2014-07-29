1effeebc1eb5392e93532e66725559c7f96b90fd
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-07-29 15:21:44 -0700

    Creating leaflet directories by location name.

--- followerLatLong.py
+++ followerLatLong.py
@@ -8,7 +8,7 @@ import os
 import errno
 from time import sleep
 from time import localtime
-from shutil import copy2
+from shutil import copy2,copytree
 from webbrowser import open_new_tab
 
 # reads access token from specified line in keyFile
@@ -21,15 +21,16 @@ access_token = keyFile.readline().rstrip()
 
 # user specified variables to influence search
 USERNAME = 'thelinq'
-MAXPAGES = 10 #maximum number of approximately 100-user pages to process
+MAXPAGES = 1 #maximum number of approximately 100-user pages to process
 # Note: This number is approximate, as some pages do not have the full 100 users
-MAXTRIES = 9 #number of pictures to go through on users' timelines to attempt to find location
+MAXTRIES = 2 #number of pictures to go through on users' timelines to attempt to find location
 #upper limit is 20 pictures to go through
 
 # output file to be used for html output and opened in web browser
 currentTime = str(localtime().tm_year) + '-' + str(localtime().tm_mon) + '-' + str(localtime().tm_mday) + '-' + str(localtime().tm_hour) + '-' + str(localtime().tm_sec)
 outputFilename = 'output_' + USERNAME + '_' + currentTime +  '.js'
-outputDirectory = os.path.dirname(os.path.realpath(__file__)) + '/LatLongData/'
+workingDirectory = os.path.dirname(os.path.realpath(__file__))
+outputDirectory = workingDirectory + '/LatLongData/'
 try:
         os.makedirs(outputDirectory)
 except OSError as exception:
@@ -56,17 +57,17 @@ def getLastLocation(userID):
 
         #Search through a user's feed for location
         tries = 0
-        for index in userFeed:
+        for eachMedia in userFeed:
             if (tries >= MAXTRIES):
                 break
-            print "Checking index of feed: " + str(index)
+            print "Checking for location in feed: " + str(eachMedia)
             try:
-                print "Found location: " + str(api.media(index.id).location.point)
+                print "Found location: " + str(api.media(eachMedia.id).location.point)
                 #Conclude the last entry and prepare for the next
                 if (publicUsers > 0):
                     outputFile.write("\n},")
-                outputFile.write("\n{\n\tlat: " + str(api.media(index.id).location.point.latitude) + 
-                        ",\n\tlon: " + str(api.media(index.id).location.point.longitude) + ",\n\t" +
+                outputFile.write("\n{\n\tlat: " + str(api.media(eachMedia.id).location.point.latitude) + 
+                        ",\n\tlon: " + str(api.media(eachMedia.id).location.point.longitude) + ",\n\t" +
                         "value: 1")
                 publicUsers += 1
                 break
@@ -86,10 +87,10 @@ def concludeOutput():
     #TODO: Add some legend data to the map
 
 # copy the .js data file into leaflet source folder
-def copyIntoLeaflet():
-    leafletMapsDirectory = os.path.dirname(os.path.realpath(__file__)) + '/leaflet_source/maps/'
-    print "\nCopying heatmap data to Leaflet directory:\n" + leafletMapsDirectory
-    copy2(outputFile.name, leafletMapsDirectory + "heatmap-data.js")
+def copyIntoLeaflet(leafletDirectory):
+    copytree(workingDirectory + '/leaflet_source/',leafletDirectory)
+    print "\nCopying heatmap data to Leaflet directory:\n" + leafletDirectory
+    copy2(outputFile.name, leafletDirectory + 'maps/' + "heatmap-data.js")
     
 initiateOutput()
 
@@ -121,10 +122,13 @@ print str(publicUsers) + " followers have location enabled (" + str(percentage) 
 
 concludeOutput()
 
-copyIntoLeaflet()
+leafletDirectory = workingDirectory + '/leaflet-' + USERNAME + '/'
+copyIntoLeaflet(leafletDirectory)
 
 print "\nSaved lat/long data to file:\n" + outputFile.name
 
 # open leaflet heatmap
-heatmapLocation = os.path.dirname(os.path.realpath(__file__)) + '/leaflet_source/maps/heatmap.html'
+heatmapLocation = leafletDirectory + 'maps/heatmap.html'
 open_new_tab("file://" + heatmapLocation)
+#TODO: Make new leaflet directory by location name
+#e.g. lflt-TheLinq
